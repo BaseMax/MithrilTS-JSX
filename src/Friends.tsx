6fb37bbdac9c83334f7dc8e7ba6bc884843aba7c
Max Base <MaxBaseCode@Gmail.Com>
2021-11-08 08:39:49 +0330

    Improved by ArthurClemens

--- src/Friends.tsx
+++ src/Friends.tsx
@@ -1,3 +1,4 @@
+ 
 import m from "mithril"
 import classes from "./Friends.module.css"
 
@@ -12,38 +13,44 @@ const Name = (elm) => {
     }
 }
 
-export const Friends = () => {
-    let names = []
-    let field_name = ""
-    const add_friend = () => {
-        field_name = field_name.trim()
-        if(field_name !== "")
-            names.push(field_name)
-        field_name = ""
-    }
-    const nameTyping = (e) => {
-        console.log(e.keyCode, e.target.value);
-        if(e.key === 'Enter' || e.keyCode === 13) {
-            add_friend()
-            e.target.value = ""
+    export const Friends = () => {
+    let names = [];
+    // let field_name = "";
+    const submit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const nameField = form.elements.name;
+        const isValid = nameField.checkValidity();
+        if (isValid) {
+            names.push(nameField.value);
         }
-        else {
-            field_name = e.target.value
-        }
+        form.reset();
     }
+
     return {
         view: () => {
         return (
-            <section className={classes.section}>
-                <h2 className={classes.sectionTitle}>Friends</h2>
-                <input type="text" onkeyup={(e) => nameTyping(e)} value={field_name} />
-                <button onclick={() => add_friend()}>Insert</button>
-                <ul>
-                    {names.map(name =>
-                        <Name name={name} />
-                    )}
-                </ul>
-            </section>
+        <section className={classes.section}>
+          <h2 className={classes.sectionTitle}>Friends</h2>
+          <form onsubmit={submit} novalidate>
+          <div>
+            <label for="id_name">Name (required)</label>
+          </div>
+          <input
+            id="id_name"
+            name="name"
+            type="text"
+            required
+            // value={field_name}
+          />
+          <button type="submit">Insert</button>
+          </form>
+          <ul>
+            {names.map((name, index) => (
+              <Name name={name} key={index} />
+            ))}
+          </ul>
+        </section>
         )},
     }
 }
